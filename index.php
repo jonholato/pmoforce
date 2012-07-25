eec78d58a925c65ea68ab0e19bf0980218abe402
jonholato <jon.holato@gmail.com>
2012-07-25 09:30:23 -0400

    Third Commit
    Updated index.php, content in html, added statcounter

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
-echo "Welcome to PMOForce, hosted on the Heroku platform!!";
+
 
 ?>
+
+<html>
+<head>
+<title>pmoforce</title>
+
+</head>
+<body>
+<p>Welcome to <b>pmoforce</b>, a PHP mash-up with Salesforce.com hosted on the Heroku platform!</p>
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=8146673; 
+var sc_invisible=1; 
+var sc_security="e960638b"; 
+</script>
+<script type="text/javascript"
+src="http://www.statcounter.com/counter/counter.js"></script>
+<noscript><div class="statcounter"><a title="tumblr visitor
+stats" href="http://statcounter.com/tumblr/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/8146673/0/e960638b/1/"
+alt="tumblr visitor stats"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+
+</body>
+</html>
